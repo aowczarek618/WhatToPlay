b83db9ba4173446b7ae19cd087888431123226c6
Artur Owczarek <oartur618@gmail.com>
2020-05-09 09:38:27 +0200

    App works, but I am going to still improve it.

--- WhatToPlay.py
+++ WhatToPlay.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import operator
 
 
 class WhatToPlay:
@@ -30,22 +31,42 @@ class WhatToPlay:
                     so to access game name I need '2' element.
                     """
             page_number += 1
-        return available_games
+        return list(available_games)
 
     # Checking metascore of a certain game
     @staticmethod
     def check_metascore(game_name):
         game_name = game_name.lower()
         metacritic_url = f"https://www.metacritic.com/game/xbox-one/{game_name}"
-        page = requests.get(metacritic_url) # Error 403
+
+        headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"}
+        page = requests.get(metacritic_url, headers=headers)
+        print(page)
         soup = BeautifulSoup(page.content, 'html.parser')
-        site_elems = soup.find()
-        print(site_elems.prettify())
+        site_elems = soup.find(class_='metascore_w xlarge game positive')
+
+        if not site_elems:
+            return 0
+        else:
+            span_elem = site_elems.find('span')
+            return int(span_elem.text)
 
     # Rating a game I played
     def rate_game(self):
         raise NotImplementedError
 
     # Recommend which game should I play
-    def recommend_me_sth(self):
-        raise NotImplementedError
+    @staticmethod
+    def recommend_me_sth():
+        available_games = WhatToPlay.get_xgp_games()
+        available_games_with_scores = {}
+        print(len(available_games))
+
+        i = 1
+        for game in available_games:
+            print(game, i)
+            available_games_with_scores[game] = WhatToPlay.check_metascore(game)
+            i += 1
+
+        return dict(sorted(available_games_with_scores.items(), key=operator.itemgetter(1), reverse=True))
+
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import WhatToPlay
 
 
+def write_to_file(my_dict):
+    with open('recommended_games.txt', 'w') as f:
+        for key, value in my_dict.items():
+            f.write(f'{key}, {value}\n')
+
+
 def main():
-    available_games = []
     what_to_play = WhatToPlay.WhatToPlay()
-    what_to_play.check_metascore("Red-Dead-Redemption-2")
+    # print(what_to_play.check_metascore("The-Witcher-3-Wild-Hunt"))
+    recommended_games = (what_to_play.recommend_me_sth())
+    write_to_file(recommended_games)
 
 
 if __name__ == '__main__':
