3b8769fb5b3e5bba7f54bb57aa6ff42e4ff0cc02
Artur Owczarek <oartur618@gmail.com>
2020-12-14 19:33:11 +0100

    Code cleanup, add generating database in html page

--- Game.py
+++ Game.py
@@ -1,19 +1,23 @@
 import re
 import time
-
-import WhatToPlay
+import logging
 import requests
-import taste_meter
+import WhatToPlay
 from bs4 import BeautifulSoup
+
+
+logger = logging.getLogger("my_logger")
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 
 class Game:
     """Game class containing basic info about certain game: name, metascore, developer, genre"""
 
-    def __init__(self, name):
+    def __init__(self, name, my_taste):
         self.name = name
-        self.metascore, self.developer, self.genre = Game.get_game_info(self)
-
+        self.metascore, self.developer, self.genres = self.get_game_info()
+        self.my_taste = my_taste
+    
     def __str__(self):
         return f'Name: {self.name}, metascore: {self.metascore}, developer: {self.developer}, genre: {self.genre}'
 
@@ -27,11 +31,13 @@ class Game:
         headers = {
             'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"}
         page = requests.get(metacritic_url, headers=headers)
-        if page.status_code == 429:
-            time.sleep(10)
+        if page.status_code == 404:
+            metacritic_url = f"https://www.metacritic.com/game/xbox-360/{self.name.lower()}"
             page = requests.get(metacritic_url, headers=headers)
         elif page.status_code == 404:
-            return 0, '-', '-'
+            time.sleep(10)
+            page = requests.get(metacritic_url, headers=headers)
+
         soup = BeautifulSoup(page.content, 'html.parser')
         site_elems = soup.find(class_=re.compile('^metascore_w xlarge game'))
 
@@ -52,12 +58,12 @@ class Game:
         return metascore, developer, genres
 
     # Compute game score given by my game taste
-    def my_score(self, my_taste):
+    @property    
+    def my_score(self):
         """Method compute MyScore"""
         my_score = self.metascore
         # Check which of genres a game is and which a developer made the game
-
-        for value, key in my_taste.items():
-            if value in self.genre or value in self.developer:
-                my_score += key
+        for key, value in self.my_taste.items():
+            if key in self.genres or key in self.developer:
+                my_score += value
         return my_score
--- main.py
+++ main.py
@@ -1,69 +1,10 @@
-import Game
 import WhatToPlay
-import pandas as pd
-import os
-import taste_meter
-
-
-def create_db(games_set, my_taste):
-    """Create database of all avaiable Xbox Game Pass games"""
-    with open('xbox_database', 'a') as f:
-        if os.stat('xbox_database').st_size == 0:
-            f.write('Name:Developer:Genre:Metascore:MyScore\n')
-
-        rated_games = rated_games_list()
-        for game in games_set:
-            if game.name not in rated_games:
-                f.write(f'{game.name}:{game.developer}:{game.genre}:{game.metascore}:{game.my_score(my_taste)}\n')
-    db = pd.read_csv('xbox_database', delimiter=':')
-    return db
-
-
-def del_unavailable_games_from_db(database, games_to_delete):
-    """Delete games from database which are currently unavailable in Xbox Game Pass subscription
-    or are currently rated."""
-    for game in games_to_delete:
-        database.drop(database[database['Name'] == game].index, inplace=True)
-    return database
-
-
-def rated_games_list():
-    """Create rated games list"""
-    rated_games = []
-    with open('rated_games', 'r') as f:
-        for line in f:
-            game_name = line.rstrip().split(':')[0]
-            rated_games.append(game_name)
-    return rated_games
-
-
-def get_old_gamenames():
-    """Get all game names which are in project folder database"""
-    old_gamenames = []
-    with open('xbox_database', 'a') as f:
-        pass  # Create file if doesn't exist, don't overwrite if exists
-    with open('xbox_database', 'r') as f:
-        for line in f:
-            gamename = line.split(':')[0]
-            if not gamename == 'Name':
-                old_gamenames.append(gamename)
-    return old_gamenames
 
 
 def main():
     """Main function"""
-    old_gamenames = get_old_gamenames()
-    rated_games = rated_games_list()
-
-    games_set, games_to_delete = WhatToPlay.get_xgp_games(old_gamenames, rated_games)
-    my_taste = taste_meter.taste_meter('rated_games')
-    db = create_db(games_set, my_taste)
-    db = del_unavailable_games_from_db(db, games_to_delete)
-
-    pd.set_option('display.max_rows', None)
-    sorted_db = db.sort_values('MyScore', ascending=False)
-    sorted_db = sorted_db[sorted_db['Metascore'] != 0]
-    print(sorted_db[['Name', 'Metascore', 'MyScore']])
+    what_to_play = WhatToPlay.WhatToPlay()
+    what_to_play.generate_html()
 
 
 if __name__ == '__main__':
