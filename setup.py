111d1079bc159257c53ce427bc714d07e0e19487
Artur Owczarek <oartur618@gmail.com>
2020-12-20 00:34:06 +0100

    Change project structure

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='xgp-reccomender',
-    version='1.0.1',
+    version='1.0.2',
     description='App searches for avaiable Xbox Game Pass games and helps to choose what to play next.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -17,7 +17,7 @@ setup(
     install_requires=['requests', 'beautifulsoup4', 'pandas'],
     entry_points={
         "console_scripts": [
-            "xgp-reccomender=main:main",
+            "xgp-reccomender=xgp_reccomender.main:main",
         ]
     },
     project_urls={
