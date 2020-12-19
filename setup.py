0d7bed12d7b19974592c208d2ea09584ebb8fe14
Artur Owczarek <oartur618@gmail.com>
2020-12-20 00:23:04 +0100

    Add entrypoints to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='xgp-reccomender',
-    version='1.0.0',
+    version='1.0.1',
     description='App searches for avaiable Xbox Game Pass games and helps to choose what to play next.',
     long_description=long_description,
     long_description_content_type='text/markdown',
